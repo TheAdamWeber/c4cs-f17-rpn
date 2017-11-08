75b4425895fa06743c62d2e0326e36a230ebb38c
ajweber <ajweber@umich.edu>
2017-11-08 13:31:23 -0500

    Updated

--- rpn.py
+++ rpn.py
@@ -1,30 +1,36 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 
 import operator
 
-ops = {
+
+operators = {
     '+': operator.add,
     '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
 }
 
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
         try:
-            stack.append(int(token))
+            token = int(token)
+            stack.append(token)
         except ValueError:
+            function = operators[token]
             arg2 = stack.pop()
             arg1 = stack.pop()
-            function = ops[token]
-            function(arg1, arg2)
             result = function(arg1, arg2)
             stack.append(result)
-    #print(stack)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
     while True:
-        calculate(input("rpn calc> "))
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
